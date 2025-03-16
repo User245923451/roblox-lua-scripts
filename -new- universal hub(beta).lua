aef77d38588143377c6dd8a65bec6bea6a72cdf1
User245923451 <ismailozkan478@gmail.com>
2025-03-16 19:50:05 +0300

    Update -new- universal hub(beta).lua

--- -new- universal hub(beta).lua
+++ -new- universal hub(beta).lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/User245923451/private-code/main/code.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/User245923451/universal-hub-script/main/universal%20hub%20code'))()
